ec7e7984825cc2f81dc04cef4b2d09fba1384fef
ZiglaCity <ziglacity@gmail.com>
2025-03-09 03:33:33 +0000

    Target showing, and positions changing onclick

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import aim from '../assets/aim-black.jpg';
 
 function Target({ position, onHit }) {
     return (
         <img
-            src="../assets/aim-black.jpg"
+            src={aim}
             alt="Target"
             className="target"
             style={{
             position: "absolute",
             left: `${position.x}px`,
             top: `${position.y}px`,
-            width: "50px",
-            height: "50px",
+            width: "100px",
+            height: "100px",
             cursor: "pointer",
             }}
             onClick={onHit}
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,8 +1,58 @@
+import { useState, useEffect } from 'react';
+import Target from '../components/Target';
+import Score from '../components/Score';
+
 function Game({difficulty}) {
+    const [score, setScore] = useState(0);
+    const [targetPosition, setTargetPosition] = useState({ x: 50, y: 50 });
+    const [timeLeft, setTimeLeft] = useState(difficulty === "easy" ? 30 : null);
+    const [targetsLeft, setTargetsLeft] = useState(difficulty === "easy" ? null : 20);
+
+    const generateNewPosition = () => {
+        const x = Math.random() * (window.innerWidth - 50);
+        const y = Math.random() * (window.innerHeight - 50);
+        setTargetPosition({ x, y });
+    };
+
+    function handleHit(){
+        setScore(score + 1);
+        generateNewPosition();
+
+        if(difficulty !== "easy"){
+            setTargetsLeft(targetsLeft - 1);
+        }
+        if (targetsLeft - 1 == 0){
+            // endgame
+        }
+    }
+
+    // in easy mode, we use the time left
+    useEffect(() => {
+        if (difficulty === "easy" && timeLeft > 0){
+            const timer = setInterval(setTimeLeft(timeLeft - 1),1000);
+            return clearTimeout(timer);
+        }
+        if (difficulty === "easy" && timeLeft === 0){
+            // endgame
+        }
+    }
+        , [timeLeft]
+    )
+
+    // if not easy mode, use targets left
+    useEffect(()=> {
+        if( difficulty !== "easy"){
+            const interval = setInterval(generateNewPosition, difficulty === "medium" ? 1000 : 500);
+            return clearInterval(interval);
+        }
+
+        }
+        ,[targetPosition]
+    )
     return (
-        <div>
-            <h1>Game</h1>
-            <p>Difficulty: {difficulty}</p>
+        <div className="game-container">
+            <Score score={score} timeLeft={timeLeft} targetsLeft={targetsLeft} />
+            <Target position={targetPosition} onHit={handleHit} />
         </div>
     );
 
